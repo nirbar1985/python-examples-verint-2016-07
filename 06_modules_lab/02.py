e8ca72fc55716de513d50c37c1b93b531b0ff60f
nirbar1985 <nirbar21@gmail.com>
2016-07-16 20:18:50 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,21 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
+"""
+write a program eivh gets in argv 2 numbers and prints its sum
+
+
+
 """
 
+import sys
+
+if len(sys.argv) != 3:
+	sys.exit("please enter exactly 2 parameters")
+
+try:
+	num1 = int(sys.argv[1])
+	num2 = int(sys.argv[2])
+
+except:
+	sys.exit("please enter only numbers as parameters")
+		
+
+print sum((num1,num2))
